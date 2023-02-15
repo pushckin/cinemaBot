8a91c980c29916732c9efde76d795d3ad4f77d8c
Владимир Амшей <pushckin@gmail.com>
2023-02-16 02:43:40 +0300

    url ttps://hdrezka.re/novinki-ordinary/ 2

--- pars.py
+++ pars.py
@@ -35,51 +35,51 @@ def hd_rezka():
     with open('new_dict.json2', 'w', encoding='utf-8') as file:
         json.dump(new_dict, file, indent=3, ensure_ascii=False)
 
-# def check_film_updet():
-#     with open("new_dict.json", encoding="utf-8") as file:
-#         new_dict = json.load(file)
-#
- #   # url = 'https://hdrezkanbnssf.net/new/'
-    # url = 'https://hdrezka.re/novinki-ordinary/'
-#     HEADERS = {
-#         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-#     }
-#
-#     r = requests.get(url, headers=HEADERS)
-#
-#     soup = BeautifulSoup(r.text, 'lxml')
-#     items = soup.find_all('div', class_='b-content__inline_item')
-#
-#     fresh_film = {}
-#     for item in items:
-#         url_id = item.find('div', class_='b-content__inline_item-link').find('a').get('href')
-#         item_id = url_id.split('/')[-1]
-#         item_id = item_id[:-5]
-#
-#         if item_id in new_dict:
-#             continue
-#         else:
-#             title = item.find('div', class_='b-content__inline_item-link').get_text()
-#             link = item.find('div', class_='b-content__inline_item-link').find('a').get('href')
-#             movie_img = item.find('div', class_='b-content__inline_item-cover').find('img').get('src')
-#
-#             new_dict[item_id] = {
-#                 'title': title,
-#                 'link': link,
-#                 'movie_img': movie_img
-#             }
-#
-#             fresh_film[item_id] = {
-#                 'title': title,
-#                 'link': link,
-#                 'movie_img': movie_img
-#             }
-#
-#     with open('new_dict.json', 'w', encoding='utf-8') as file:
-#         json.dump(new_dict, file, indent=3, ensure_ascii=False)
-#
-#     return fresh_film
+def check_film_updet():
+    with open("new_dict.json", encoding="utf-8") as file:
+        new_dict = json.load(file)
+
+   # url = 'https://hdrezkanbnssf.net/new/'
+    url = 'https://hdrezka.re/novinki-ordinary/'
+    HEADERS = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    }
+
+    r = requests.get(url, headers=HEADERS)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    items = soup.find_all('div', class_='b-content__inline_item')
+
+    fresh_film = {}
+    for item in items:
+        url_id = item.find('div', class_='b-content__inline_item-link').find('a').get('href')
+        item_id = url_id.split('/')[-1]
+        item_id = item_id[:-5]
+
+        if item_id in new_dict:
+            continue
+        else:
+            title = item.find('div', class_='b-content__inline_item-link').get_text()
+            link = item.find('div', class_='b-content__inline_item-link').find('a').get('href')
+            movie_img = item.find('div', class_='b-content__inline_item-cover').find('img').get('src')
+
+            new_dict[item_id] = {
+                'title': title,
+                'link': link,
+                'movie_img': movie_img
+            }
+
+            fresh_film[item_id] = {
+                'title': title,
+                'link': link,
+                'movie_img': movie_img
+            }
+
+    with open('new_dict.json', 'w', encoding='utf-8') as file:
+        json.dump(new_dict, file, indent=3, ensure_ascii=False)
+
+    return fresh_film
 
 def main():
     hd_rezka()
